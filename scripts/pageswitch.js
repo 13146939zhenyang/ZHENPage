bc824361122d5fbefbeea799890477816c5aaae5
Zhen Yang <webyangfullstack@gmail.com>
2021-01-11 18:04:36 +1100

    input button readonly

--- scripts/pageswitch.js
+++ scripts/pageswitch.js
@@ -1,8 +1,10 @@
-var pw = new pageSwitch('pageswitch', {
+var pageSwitch = require('pageswitch');
+
+var pw = new pageSwitch('#pages', {
     duration: 600, //int 页面过渡时间
     direction: 1, //int 页面切换方向，0横向，1纵向
     start: 0, //int 默认显示页面
-    loop: false, //bool 是否循环切换
+    loop: true, //bool 是否循环切换
     ease: 'ease', //string|function 过渡曲线动画，详见下方说明
     transition: 'slide', //string|function转场方式，详见下方说明
     freeze: false, //bool 是否冻结页面（冻结后不可响应用户操作，可以通过 `.freeze(false)` 方法来解冻）
@@ -13,6 +15,6 @@ var pw = new pageSwitch('pageswitch', {
     interval: int //bool 幻灯播放时间间隔 新增
 });
 
-function a() {
-
+function nextP() {
+    pw.next();
 }

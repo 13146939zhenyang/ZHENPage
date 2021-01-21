273e824e12e829336bc941cf9b0099d0bab35a64
Zhen Yang <webyangfullstack@gmail.com>
2021-01-21 18:11:43 +1100

    uodate page switch animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,38 @@
+function homeChangeOpacity() {
+    document.querySelector('.body-resume').style.opacity = '0';
+    document.querySelector('.body-projects').style.opacity = '0';
+    document.querySelector('.body-blog').style.opacity = '0';
+    document.querySelector('.body-contact').style.opacity = '0';
+}
+
+function resumeChangeOpacity() {
+    document.querySelector('.body-home').style.opacity = '0';
+    document.querySelector('.body-projects').style.opacity = '0';
+    document.querySelector('.body-blog').style.opacity = '0';
+    document.querySelector('.body-contact').style.opacity = '0';
+}
+
+function projectsChangeOpacity() {
+    document.querySelector('.body-resume').style.opacity = '0';
+    document.querySelector('.body-home').style.opacity = '0';
+    document.querySelector('.body-blog').style.opacity = '0';
+    document.querySelector('.body-contact').style.opacity = '0';
+}
+
+function blogChangeOpacity() {
+    document.querySelector('.body-resume').style.opacity = '0';
+    document.querySelector('.body-projects').style.opacity = '0';
+    document.querySelector('.body-home').style.opacity = '0';
+    document.querySelector('.body-contact').style.opacity = '0';
+}
+
+function contactChangeOpacity() {
+    document.querySelector('.body-resume').style.opacity = '0';
+    document.querySelector('.body-projects').style.opacity = '0';
+    document.querySelector('.body-blog').style.opacity = '0';
+    document.querySelector('.body-home').style.opacity = '0';
+}
+
 function home() {
     document.querySelector('.body-home').style.zIndex = '2000';
     document.querySelector('.body-resume').style.zIndex = '1000';
@@ -7,16 +42,42 @@ function home() {
     // animation
     document.querySelector('.body-home').style.opacity = '1';
     document.querySelector('.body-home').style.animationName = 'pageCome';
-    document.querySelector('.body-home').style.animationDuration = '1.5s';
+    document.querySelector('.body-home').style.animationDuration = '1s';
     document.querySelector('.body-resume').style.animationName = 'none';
     document.querySelector('.body-projects').style.animationName = 'none';
     document.querySelector('.body-blog').style.animationName = 'none';
     document.querySelector('.body-contact').style.animationName = 'none';
-    // document.querySelector('.body-home').style.animationIterationCount = 'infinite';
-    document.querySelector('.body-resume').style.opacity = '0';
-    document.querySelector('.body-projects').style.opacity = '0';
-    document.querySelector('.body-blog').style.opacity = '0';
-    document.querySelector('.body-contact').style.opacity = '0';
+
+
+    var re = document.querySelector('.body-resume').style.opacity
+    if (re == '1') {
+        document.querySelector('.body-resume').style.animationName = 'pageGo';
+        document.querySelector('.body-resume').style.animationDuration = '1s';
+        homeChangeOpacity();
+        console.log('success yz')
+    }
+    var pr = document.querySelector('.body-projects').style.opacity
+    if (pr == '1') {
+        document.querySelector('.body-projects').style.animationName = 'pageGo';
+        document.querySelector('.body-projects').style.animationDuration = '1s';
+        homeChangeOpacity();
+        console.log('success yz')
+    }
+    var bl = document.querySelector('.body-blog').style.opacity
+    if (bl == '1') {
+        document.querySelector('.body-blog').style.animationName = 'pageGo';
+        document.querySelector('.body-blog').style.animationDuration = '1s';
+        homeChangeOpacity();
+        console.log('success yz')
+    }
+    var co = document.querySelector('.body-contact').style.opacity
+    if (co == '1') {
+        document.querySelector('.body-contact').style.animationName = 'pageGo';
+        document.querySelector('.body-contact').style.animationDuration = '1s';
+        homeChangeOpacity();
+        console.log('success yz')
+    }
+
     document.querySelector('.header-navbar-item-home').style.color = '#595959';
     document.querySelector('.header-navbar-item-home-line').style.opacity = '1';
     document.querySelector('.header-navbar-item-resume').style.color = '#9a9a9a';
@@ -31,27 +92,50 @@ function home() {
 }
 
 function resume() {
-    // animation
-    document.querySelector('.body-resume').style.opacity = '1';
-    document.querySelector('.body-resume').style.animationName = 'pageCome';
-    document.querySelector('.body-resume').style.animationDuration = '1.5s';
-    document.querySelector('.body-home').style.animationName = 'none';
-    document.querySelector('.body-projects').style.animationName = 'none';
-    document.querySelector('.body-blog').style.animationName = 'none';
-    document.querySelector('.body-contact').style.animationName = 'none';
-
-
     document.querySelector('.body-home').style.zIndex = '1000';
     document.querySelector('.body-resume').style.zIndex = '2000';
     document.querySelector('.body-projects').style.zIndex = '1000';
     document.querySelector('.body-blog').style.zIndex = '1000';
     document.querySelector('.body-contact').style.zIndex = '1000';
-
-    document.querySelector('.body-home').style.opacity = '0';
+    // animation
     document.querySelector('.body-resume').style.opacity = '1';
-    document.querySelector('.body-projects').style.opacity = '0';
-    document.querySelector('.body-blog').style.opacity = '0';
-    document.querySelector('.body-contact').style.opacity = '0';
+    document.querySelector('.body-resume').style.animationName = 'pageCome';
+    document.querySelector('.body-resume').style.animationDuration = '1s';
+    document.querySelector('.body-home').style.animationName = 'none';
+    document.querySelector('.body-projects').style.animationName = 'none';
+    document.querySelector('.body-blog').style.animationName = 'none';
+    document.querySelector('.body-contact').style.animationName = 'none';
+
+    var rer = document.querySelector('.body-home').style.opacity
+    if (rer == '1') {
+        document.querySelector('.body-home').style.animationName = 'pageGo';
+        document.querySelector('.body-home').style.animationDuration = '1s';
+        resumeChangeOpacity();
+        console.log('success yz')
+    }
+    var prr = document.querySelector('.body-projects').style.opacity
+    if (prr == '1') {
+        document.querySelector('.body-projects').style.animationName = 'pageGo';
+        document.querySelector('.body-projects').style.animationDuration = '1s';
+        resumeChangeOpacity();
+        console.log('success yz')
+    }
+    var blr = document.querySelector('.body-blog').style.opacity
+    if (blr == '1') {
+        document.querySelector('.body-blog').style.animationName = 'pageGo';
+        document.querySelector('.body-blog').style.animationDuration = '1s';
+        resumeChangeOpacity();
+        console.log('success yz')
+    }
+    var cor = document.querySelector('.body-contact').style.opacity
+    if (cor == '1') {
+        document.querySelector('.body-contact').style.animationName = 'pageGo';
+        document.querySelector('.body-contact').style.animationDuration = '1s';
+        resumeChangeOpacity();
+        console.log('success yz')
+    }
+
+
 
     document.querySelector('.header-navbar-item-home').style.color = '#9a9a9a';
     document.querySelector('.header-navbar-item-home-line').style.opacity = '0';
@@ -69,7 +153,7 @@ function projects() {
     // animation
     document.querySelector('.body-projects').style.opacity = '1';
     document.querySelector('.body-projects').style.animationName = 'pageCome';
-    document.querySelector('.body-projects').style.animationDuration = '1.5s';
+    document.querySelector('.body-projects').style.animationDuration = '1s';
     document.querySelector('.body-home').style.animationName = 'none';
     document.querySelector('.body-resume').style.animationName = 'none';
     document.querySelector('.body-blog').style.animationName = 'none';
@@ -81,11 +165,34 @@ function projects() {
     document.querySelector('.body-blog').style.zIndex = '1000';
     document.querySelector('.body-contact').style.zIndex = '1000';
 
-    document.querySelector('.body-home').style.opacity = '0';
-    document.querySelector('.body-resume').style.opacity = '0';
-    document.querySelector('.body-projects').style.opacity = '1';
-    document.querySelector('.body-blog').style.opacity = '0';
-    document.querySelector('.body-contact').style.opacity = '0';
+    var rer = document.querySelector('.body-home').style.opacity
+    if (rer == '1') {
+        document.querySelector('.body-home').style.animationName = 'pageGo';
+        document.querySelector('.body-home').style.animationDuration = '1s';
+        projectsChangeOpacity();
+        console.log('success yz')
+    }
+    var prr = document.querySelector('.body-resume').style.opacity
+    if (prr == '1') {
+        document.querySelector('.body-resume').style.animationName = 'pageGo';
+        document.querySelector('.body-resume').style.animationDuration = '1s';
+        projectsChangeOpacity();
+        console.log('success yz')
+    }
+    var blr = document.querySelector('.body-blog').style.opacity
+    if (blr == '1') {
+        document.querySelector('.body-blog').style.animationName = 'pageGo';
+        document.querySelector('.body-blog').style.animationDuration = '1s';
+        projectsChangeOpacity();
+        console.log('success yz')
+    }
+    var cor = document.querySelector('.body-contact').style.opacity
+    if (cor == '1') {
+        document.querySelector('.body-contact').style.animationName = 'pageGo';
+        document.querySelector('.body-contact').style.animationDuration = '1s';
+        projectsChangeOpacity();
+        console.log('success yz')
+    }
     document.querySelector('.header-navbar-item-home').style.color = '#9a9a9a';
     document.querySelector('.header-navbar-item-home-line').style.opacity = '0';
     document.querySelector('.header-navbar-item-resume').style.color = '#9a9a9a';
@@ -102,7 +209,7 @@ function blog() {
     // animation
     document.querySelector('.body-blog').style.opacity = '1';
     document.querySelector('.body-blog').style.animationName = 'pageCome';
-    document.querySelector('.body-blog').style.animationDuration = '1.5s';
+    document.querySelector('.body-blog').style.animationDuration = '1s';
     document.querySelector('.body-home').style.animationName = 'none';
     document.querySelector('.body-resume').style.animationName = 'none';
     document.querySelector('.body-projects').style.animationName = 'none';
@@ -114,11 +221,34 @@ function blog() {
     document.querySelector('.body-blog').style.zIndex = '2000';
     document.querySelector('.body-contact').style.zIndex = '1000';
 
-    document.querySelector('.body-home').style.opacity = '0';
-    document.querySelector('.body-resume').style.opacity = '0';
-    document.querySelector('.body-projects').style.opacity = '0';
-    document.querySelector('.body-blog').style.opacity = '1';
-    document.querySelector('.body-contact').style.opacity = '0';
+    var rer = document.querySelector('.body-home').style.opacity
+    if (rer == '1') {
+        document.querySelector('.body-home').style.animationName = 'pageGo';
+        document.querySelector('.body-home').style.animationDuration = '1s';
+        blogChangeOpacity();
+        console.log('success yz')
+    }
+    var prr = document.querySelector('.body-resume').style.opacity
+    if (prr == '1') {
+        document.querySelector('.body-resume').style.animationName = 'pageGo';
+        document.querySelector('.body-resume').style.animationDuration = '1s';
+        blogChangeOpacity();
+        console.log('success yz')
+    }
+    var blr = document.querySelector('.body-projects').style.opacity
+    if (blr == '1') {
+        document.querySelector('.body-projects').style.animationName = 'pageGo';
+        document.querySelector('.body-projects').style.animationDuration = '1s';
+        blogChangeOpacity();
+        console.log('success yz')
+    }
+    var cor = document.querySelector('.body-contact').style.opacity
+    if (cor == '1') {
+        document.querySelector('.body-contact').style.animationName = 'pageGo';
+        document.querySelector('.body-contact').style.animationDuration = '1s';
+        blogChangeOpacity();
+        console.log('success yz')
+    }
     document.querySelector('.header-navbar-item-home').style.color = '#9a9a9a';
     document.querySelector('.header-navbar-item-home-line').style.opacity = '0';
     document.querySelector('.header-navbar-item-resume').style.color = '#9a9a9a';
@@ -135,7 +265,7 @@ function contact() {
     // animation
     document.querySelector('.body-contact').style.opacity = '1';
     document.querySelector('.body-contact').style.animationName = 'pageCome';
-    document.querySelector('.body-contact').style.animationDuration = '1.5s';
+    document.querySelector('.body-contact').style.animationDuration = '1s';
     document.querySelector('.body-home').style.animationName = 'none';
     document.querySelector('.body-resume').style.animationName = 'none';
     document.querySelector('.body-projects').style.animationName = 'none';
@@ -148,11 +278,34 @@ function contact() {
     document.querySelector('.body-blog').style.zIndex = '1000';
     document.querySelector('.body-contact').style.zIndex = '2000';
 
-    document.querySelector('.body-home').style.opacity = '0';
-    document.querySelector('.body-resume').style.opacity = '0';
-    document.querySelector('.body-projects').style.opacity = '0';
-    document.querySelector('.body-blog').style.opacity = '0';
-    document.querySelector('.body-contact').style.opacity = '1';
+    var rer = document.querySelector('.body-home').style.opacity
+    if (rer == '1') {
+        document.querySelector('.body-home').style.animationName = 'pageGo';
+        document.querySelector('.body-home').style.animationDuration = '1s';
+        contactChangeOpacity();
+        console.log('success yz')
+    }
+    var prr = document.querySelector('.body-resume').style.opacity
+    if (prr == '1') {
+        document.querySelector('.body-resume').style.animationName = 'pageGo';
+        document.querySelector('.body-resume').style.animationDuration = '1s';
+        contactChangeOpacity();
+        console.log('success yz')
+    }
+    var blr = document.querySelector('.body-projects').style.opacity
+    if (blr == '1') {
+        document.querySelector('.body-projects').style.animationName = 'pageGo';
+        document.querySelector('.body-projects').style.animationDuration = '1s';
+        contactChangeOpacity();
+        console.log('success yz')
+    }
+    var cor = document.querySelector('.body-blog').style.opacity
+    if (cor == '1') {
+        document.querySelector('.body-blog').style.animationName = 'pageGo';
+        document.querySelector('.body-blog').style.animationDuration = '1s';
+        contactChangeOpacity();
+        console.log('success yz')
+    }
     document.querySelector('.header-navbar-item-home').style.color = '#9a9a9a';
     document.querySelector('.header-navbar-item-home-line').style.opacity = '0';
     document.querySelector('.header-navbar-item-resume').style.color = '#9a9a9a';
